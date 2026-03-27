ea82ec18e55dc42d8be0e62b735f7056d6818fb2
MithicSpirit <rpc01234@gmail.com>
2026-03-27 01:39:51 -0400

    add sorting surfaces within layer

--- niri-config/src/lib.rs
+++ niri-config/src/lib.rs
@@ -935,6 +935,7 @@ mod tests {
 
             layer-rule {
                 match namespace="^notifications$"
+                priority 1
                 block-out-from "screencast"
             }
 
@@ -1958,6 +1959,9 @@ mod tests {
                     ],
                     excludes: [],
                     opacity: None,
+                    priority: Some(
+                        1,
+                    ),
                     block_out_from: Some(
                         Screencast,
                     ),
