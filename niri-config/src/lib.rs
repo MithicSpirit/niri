c9ada460b502f320e03f4cd6ffe568d41717aece
MithicSpirit <rpc01234@gmail.com>
2026-05-27 17:24:20 -0400

    add layout option to maximize columns by default
    With the "maximize" property on layout{default-column-width}, windows in
    the layout will spawn maximized, as if from a window rule.  This
    behavior is overriden by window rules.

--- niri-config/src/lib.rs
+++ niri-config/src/lib.rs
@@ -824,7 +824,7 @@ mod tests {
                     fixed 1280
                 }
 
-                default-column-width { proportion 0.25; }
+                default-column-width maximize=true { proportion 0.25; }
 
                 gaps 8
 
@@ -1472,11 +1472,14 @@ mod tests {
                         1280,
                     ),
                 ],
-                default_column_width: Some(
-                    Proportion(
-                        0.25,
+                default_column_width: DefaultColumnWidth {
+                    size: Some(
+                        Proportion(
+                            0.25,
+                        ),
                     ),
-                ),
+                    maximize: true,
+                },
                 preset_window_heights: [
                     Proportion(
                         0.25,
